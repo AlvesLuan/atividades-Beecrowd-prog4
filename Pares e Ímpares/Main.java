fe77d0e5e714b5e12e916e72ddc434cf1a37f57d
Luan K. Alves <luankauan10@gmail.com>
2024-11-24 17:55:55 -0300

    update-metodo-organizarArray.java

--- Pares e Ímpares/Main.java
+++ Pares e Ímpares/Main.java
@@ -2,35 +2,40 @@ import java.io.IOException;
 import java.util.*;
  
 public class Main {
-    public static void organizarVetor(int[] vetor){
-        int tamanho = vetor.length;
+    public static void organizarArray(int[] array){
+        Arrays.sort(array);
+
+        /*
+        int tamanho = array.length;
+        //bubbleSort não pode, mt pesadin, time exceed.
+         
         for (int i = 0; i < tamanho - 1; i++) {
             for (int j = 0; j < tamanho - 1 - i; j++) {
-                if (vetor[j] > vetor[j + 1]) {
-                    int aux = vetor[j];
-                    vetor[j] = vetor[j + 1];
-                    vetor[j + 1] = aux;
+                if (array[j] > array[j + 1]) {
+                    int aux = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = aux;
                 }
             }
-        }
+        } */
     }
 
-    public static void mostraVetorOrganizado(int[] vetor){
-        for (int i = 0; i < vetor.length; i++){
-            System.out.println(vetor[i]);
+    public static void mostraarrayOrganizado(int[] array){
+        for (int i = 0; i < array.length; i++){
+            System.out.println(array[i]);
         }
     }
 
     public static void main(String[] args) throws IOException {
         Scanner Entrada = new Scanner(System.in);
 
-        int tamanhoVetor = Entrada.nextInt();
-        int vetorDeNumeros[] = new int[tamanhoVetor];
+        int tamanhoArray = Entrada.nextInt();
+        int arrayDeNumeros[] = new int[tamanhoArray];
  
-        for (int i = 0; i < vetorDeNumeros.length; i++){
-            vetorDeNumeros[i] = Entrada.nextInt();
+        for (int i = 0; i < arrayDeNumeros.length; i++){
+            arrayDeNumeros[i] = Entrada.nextInt();
         }
-        organizarVetor(vetorDeNumeros);
-        mostraVetorOrganizado(vetorDeNumeros);
+        organizarArray(arrayDeNumeros);
+        mostraarrayOrganizado(arrayDeNumeros);
     }
 }
