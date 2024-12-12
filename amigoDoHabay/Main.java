28248d422e03381ae83f301f81576d3f21762e69
Luan K. Alves <luankauan10@gmail.com>
2024-12-11 22:24:14 -0300

    Update Main.java

--- amigoDoHabay/Main.java
+++ amigoDoHabay/Main.java
@@ -2,25 +2,68 @@ import java.io.IOException;
 import java.util.*;
 
 public class Main {
+
+    public static void organiarStringArrayList(ArrayList<String> arrayListAmigos){
+        Collections.sort(arrayListAmigos);
+    }
+
+    public static void mostraArrayList(ArrayList<String> arrayListAmigos){
+        for (String amigo : arrayListAmigos) {
+            System.out.println(amigo);
+        }
+    }
+    
+
     public static void main(String[] args) throws IOException {
+        ArrayList<String> amigosSIM = new ArrayList<String>();
+        ArrayList<String> amigosNAO = new ArrayList<String>();
         Scanner Entrada = new Scanner(System.in);
-        boolean repeticao = true;
-        do{
+        String amigoAtual = null;
+
+        while (true) {
             String linha = Entrada.nextLine();
 
             if (linha.equals("FIM")) {
-                repeticao = false;
                 break;
             }
-            
+
             String[] partes = linha.split(" ");
             String nome = partes[0];
             String situacao = partes[1];
 
-            System.out.println(nome+" E "+situacao);
-        }while (repeticao);
 
-        System.out.println("SAIU") ;
+
+            if (amigoAtual == null){
+                if (situacao.equals("YES")){
+                    amigoAtual = nome;
+                }
+                
+            } else if (situacao.equals("YES") && (amigoAtual != null) ){
+                if (amigoAtual.length() > nome.length()){
+                    amigoAtual = nome;
+                }
+            }
+
+
+            if (situacao.equals("YES")){
+                if(!amigosSIM.contains(nome)){
+                    amigosSIM.add(nome);
+                }
+            }
+
+            if (situacao.equals("NO")){
+                if(!amigosNAO.contains(nome)){
+                    amigosNAO.add(nome);
+                }
+            }
+            
+        };
+        organiarStringArrayList(amigosSIM);
+        organiarStringArrayList(amigosNAO);
+        mostraArrayList(amigosSIM);
+        mostraArrayList(amigosNAO);
+
+        System.out.println("\nAmigo do Habay:\n"+amigoAtual);
 
     }
 }
