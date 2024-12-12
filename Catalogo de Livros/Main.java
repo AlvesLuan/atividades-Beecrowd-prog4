5b5d0320c608a1280273bf73e0ae068444ce6396
Luan K. Alves <luankauan10@gmail.com>
2024-12-12 15:41:36 -0300

    Update Main.java

--- Catalogo de Livros/Main.java
+++ Catalogo de Livros/Main.java
@@ -1,25 +1,60 @@
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Scanner;
  
 public class Main {
+
+    public static void printar(int[] arrayDeMaiores){
+        for (int num : arrayDeMaiores) {
+            System.out.println(num);
+        }
+    }
+
+    public static void somaMaiores(int[] arrayDeMaiores){
+        int resultado = 0;
+        for (int u = 0; u < arrayDeMaiores.length ; u++){
+            resultado += arrayDeMaiores[u];
+        }
+        System.out.println(resultado);
+    }
  
     public static void main(String[] args) throws IOException {
         Scanner Entrada = new Scanner(System.in);
-        String linha = Entrada.nextLine();
+        int linhas = 0;
+        int totalLinhas = 5;
+        int arrayDeMaiores[] = new int[5];
         
-        // 5 2 5 6 3 8
-        String[] partes = linha.split(" ");
-        int qtdLivros = Integer.parseInt(partes[0]); // Converte para inteiro
-        int arrayDeValores[] = new int[qtdLivros];
-        for (int i = 1; i < arrayDeValores.length; i++){
-            arrayDeValores[i] = Integer.parseInt(partes[i]);
-        }
+        /*  entradas a testar:
+            5 2 5 6 3 8 
+            5 9 6 3 1 5
+            5 4 8 5 2 6
+            5 3 2 4 9 5
+            5 7 8 5 1 4     */
 
-        System.out.print(qtdLivros+" ");
-        for (int num : arrayDeValores) {
-            System.out.print(num+" ");
+        while (linhas < totalLinhas) {
+            String linha = Entrada.nextLine();
+            String partes[] = linha.split(" ");
+            
+            int qtdLivros = Integer.parseInt(partes[0]);
+            int arrayDeValores[] = new int[qtdLivros];
+
+            for (int i = 0; i < qtdLivros && i + 1 < partes.length; i++) {
+                arrayDeValores[i] = Integer.parseInt(partes[i + 1]);
+            }
+
+            int maiorValor = Integer.MIN_VALUE;
+            for (int valor : arrayDeValores) {
+                if (valor > maiorValor) {
+                    maiorValor = valor;
+                }
+            }
+            arrayDeMaiores[linhas] = maiorValor;
+
+            linhas++;
         }
-        
+        int ultimaLinha = Entrada.nextInt();
+
+        somaMaiores(arrayDeMaiores);
+        //printar(arrayDeMaiores);
     }
- 
 }
